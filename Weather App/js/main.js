3c234393c5f47acf5c44898e33c66fddb067294f
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-23 11:53:26 +0300

    Get Geolocation Data

--- Weather App/js/main.js
+++ Weather App/js/main.js
@@ -1,3 +1,20 @@
 window.addEventListener('load', () => {
-    
+    let long;
+    let lat;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/2fc38197207d3341746d1911864cd14c/${lat},${long}`;
+
+            fetch(api).then(data => {
+                return data.json();
+            }).then(data => {
+                console.log(data);
+            })
+        });
+    }
 })
