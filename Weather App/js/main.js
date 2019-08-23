1c7ca4fa4bf70b3ec842b04e2ce98b321e0580bd
aleksandar9999a <aleksandar9999a@gmail.com>
2019-08-23 12:40:43 +0300

    setIcon Function

--- Weather App/js/main.js
+++ Weather App/js/main.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-describtion');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let temperatureTimezone = document.querySelector('.location-timezone');
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -13,8 +16,21 @@ window.addEventListener('load', () => {
             fetch(api).then(data => {
                 return data.json();
             }).then(data => {
-                console.log(data);
+                const {temperature, summary, icon} = data.currently;
+                const timezone = data.timezone;
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                temperatureTimezone.textContent = timezone;
+
+                setIcon(icon, document.querySelector('.icon'));
             })
         });
+    }
+
+    function setIcon(icon, iconID) {
+        const skycons = new Skycons({color: "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 })
