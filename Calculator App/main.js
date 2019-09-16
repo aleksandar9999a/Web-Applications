8e5f5d8b7189a9f5db490e9635df9755677608fd
aleksandar9999a <aleksandar9999a@gmail.com>
2019-09-16 11:01:36 +0300

    Added Calculation Function

--- Calculator App/main.js
+++ Calculator App/main.js
@@ -2,22 +2,49 @@ let numberArr = [];
 
 const calculation = (numberArr, operator) => {
     if (operator == '+') {
-        return a + b;
+        let number = 0;
+
+        numberArr.forEach(e => {number += e;});
+
+        return number;
     }
     else if (operator == '-') {
-        return a - b;
+        let number = numberArr[0];
+
+        for (let i = 1; i < numberArr.length; i++) {
+            number -= numberArr[i];
+        }
+
+        return number;
     }
     else if (operator == '*') {
-        return a * b;
+        let number = 1;
+
+        for (let i = 0; i < numberArr.length; i++) {
+            number *= numberArr[i];
+        }
+
+        return number;
     }
     else if (operator == '/') {
-        return a / b;
+        let number = numberArr[0];
+
+        for (let i = 1; i < numberArr.length; i++) {
+            number /= numberArr[i];
+        }
+
+        return number;
     }
 }
 
 const getNumberFromDisplay = (operator) => {
     let currentNumber = document.querySelector('.calculator-screen').value;
     numberArr.push(currentNumber);
+    document.querySelector('.calculator-screen').value = 0;
+    let newNumber = calculation(numberArr, operator);
+    numberArr = [newNumber];
+    console.log(numberArr);
+    
 }
 
 const addToDisplay = (number) => {
